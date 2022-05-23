adfcac947dd943f1002c5d804689f8031525325f
Marli Coetzee <mareliefies@gmail.com>
2022-05-23 15:35:01 +0200

    Making js uniform

--- amd/build/externalpractice.min.js
+++ amd/build/externalpractice.min.js
@@ -6,11 +6,9 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
       external_token,
       activityid,
       responseid,
-      idsq,
-      currenturl,
-      next_id,
-      $siyavula_activity_id,
-      show_retry_btn
+      show_retry_btn,
+      idqt,
+      seedqt
     ) {
       $(document).ready(function () {
         // Initialise MathJax typesetting
@@ -19,138 +17,151 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
 
         show_retry_btn = parseInt(show_retry_btn);
         $(".question-content").on("click", function (e) {
-          const response = e.currentTarget.dataset.response;
-          const targetid = e.currentTarget.id;
           if (
             e.target.className ===
             "sv-button sv-button--primary check-answer-button"
           ) {
             e.preventDefault();
-            var formData = $(
-              `div#${targetid} form[name="questions"]`
-            ).serialize();
-            var submitresponse = Ajax.call([
-              {
-                methodname: "filter_siyavula_submit_answers_siyavula",
-                args: {
-                  baseurl: baseurl,
-                  token: token,
-                  external_token: external_token,
-                  activityid: targetid,
-                  responseid: response,
-                  data: formData,
+
+            setTimeout(() => {
+              const response = e.currentTarget.dataset.response;
+              const targetid = e.currentTarget.id;
+              var formData = $(
+                `div#${targetid} form[name='questions']`
+              ).serialize();
+              var submitresponse = Ajax.call([
+                {
+                  methodname: "filter_siyavula_submit_answers_siyavula",
+                  args: {
+                    baseurl: baseurl,
+                    token: token,
+                    external_token: external_token,
+                    activityid: activityid,
+                    responseid: responseid,
+                    data: formData,
+                  },
                 },
-              },
-            ]);
-            submitresponse[0]
-              .done(function (response) {
-                var dataresponse = JSON.parse(response.response);
-                var html = dataresponse.response.question_html;
-                let timest = Math.floor(Date.now() / 1000);
-                html = html.replaceAll(
-                  "sv-button toggle-solution",
-                  `sv-button toggle-solution btnsolution-${targetid}-${timest}`
-                );
-                $(`#${targetid}.question-content`).html(html);
-                $(`div#${targetid} .toggle-solution-checkbox`).css(
-                  "visibility",
-                  "hidden"
-                );
-
-                const retry = document.querySelector('a[name="retry"]');
-                if (retry) {
-                  retry.setAttribute(
-                    "href",
-                    location.href +
-                      (location.href.includes("?") ? "&" : "?") +
-                      "aid=" +
-                      activityid +
-                      "&" +
-                      "rid=" +
-                      responseid
+              ]);
+              submitresponse[0]
+                .done(function (response) {
+                  var dataresponse = JSON.parse(response.response);
+                  var html = dataresponse.response.question_html;
+                  let timest = Math.floor(Date.now() / 1000);
+                  html = html.replaceAll(
+                    "sv-button toggle-solution",
+                    `sv-button toggle-solution btnsolution-${targetid}-${timest}`
                   );
-                }
-
-                const nextqt = document.querySelector('a[name="nextPage"]');
-                if (nextqt) {
-                  nextqt.setAttribute(
-                    "href",
-                    location.href +
-                      (location.href.includes("?") ? "&" : "?") +
-                      "sectionid=" +
-                      idqt +
-                      "&" +
-                      "seed=" +
-                      seedqt
+                  $(`#${targetid}.question-content`).html(html);
+                  $(`div#${targetid} .toggle-solution-checkbox`).css(
+                    "visibility",
+                    "hidden"
                   );
-                }
-
-                const theId = targetid;
-                const escapeID = CSS.escape(theId);
-
-                const labelsSolution = document.querySelectorAll(
-                  `#${escapeID}.question-content #show-hide-solution`
-                );
-
-                labelsSolution.forEach((labelSolution, key) => {
-                  labelSolution.innerHTML = "";
-
-                  const newShowSpan = document.createElement("input");
-                  newShowSpan.classList.add("sv-button");
-                  newShowSpan.value = "Show the full solution";
-                  newShowSpan.type = "button";
-                  newShowSpan.id = `show${key}`;
-
-                  const newHideSpan = document.createElement("input");
-                  newHideSpan.value = "Hide the full solution";
-                  newHideSpan.classList.add("sv-button");
-                  newHideSpan.type = "button";
-                  newHideSpan.id = `hide${key}`;
-
-                  var is_correct = true;
-                  const rsElement = labelSolution.nextSibling; // Response information
-                  const identificador = `${rsElement.id}-${key}`;
-                  rsElement.classList.add(identificador);
-                  console.log(rsElement);
-                  if (rsElement.id == "correct-solution") {
-                    is_correct = true;
-                  } else {
-                    is_correct = false;
+                  $(".sv-button.sv-button--goto-question").css(
+                    "display",
+                    "inline-block"
+                  );
+                  const retry = document.querySelector('a[name="retry"]');
+                  if (retry) {
+                    retry.setAttribute(
+                      "href",
+                      location.href +
+                        (location.href.includes("?") ? "&" : "?") +
+                        "aid=" +
+                        activityid +
+                        "&" +
+                        "rid=" +
+                        responseid
+                    );
+                    if (!show_retry_btn) {
+                      // Hide the btn
+                      console.log("hide");
+                      retry.style.display = "none";
+                    }
                   }
 
-                  if (is_correct == false) {
-                    //$(`div#${targetid} span:contains('Show the full solution')`).css("display", "none");
-                    newShowSpan.style.display = "none";
-                  } else {
-                    //$(`div#${targetid} span:contains('Hide the full solution')`).css("display", "none");
-                    newHideSpan.style.display = "none";
+                  const nextqt = document.querySelector('a[name="nextPage"]');
+                  if (nextqt) {
+                    nextqt.setAttribute(
+                      "href",
+                      location.href +
+                        (location.href.includes("?") ? "&" : "?") +
+                        "sectionid=" +
+                        idqt +
+                        "&" +
+                        "seed=" +
+                        seedqt
+                    );
                   }
-                  labelSolution.append(newShowSpan);
-                  labelSolution.append(newHideSpan);
 
-                  const spanShow = labelSolution.querySelector(`#show${key}`);
-                  const spanHide = labelSolution.querySelector(`#hide${key}`);
-                  const functionClickSolution = (btnE) => {
-                    const currentSpan = btnE.target;
-                    if (currentSpan.value.includes("Show")) {
-                      spanShow.style.display = "none";
-                      spanHide.style.display = "initial";
+                  const theId = targetid;
+                  console.log(theId);
+                  const escapeID = CSS.escape(theId);
+
+                  const labelsSolution = document.querySelectorAll(
+                    `#${escapeID}.question-content #show-hide-solution`
+                  );
+                  console.log(labelsSolution);
+
+                  labelsSolution.forEach((labelSolution, key) => {
+                    labelSolution.innerHTML = "";
+
+                    const newShowSpan = document.createElement("input");
+                    newShowSpan.classList.add("sv-button");
+                    newShowSpan.value = "Show the full solution";
+                    newShowSpan.type = "button";
+                    newShowSpan.id = `show${key}`;
+
+                    const newHideSpan = document.createElement("input");
+                    newHideSpan.value = "Hide the full solution";
+                    newHideSpan.classList.add("sv-button");
+                    newHideSpan.type = "button";
+                    newHideSpan.id = `hide${key}`;
+
+                    var is_correct = true;
+                    const rsElement = labelSolution.nextSibling; // Response information
+                    const identificador = `${rsElement.id}-${key}`;
+                    rsElement.classList.add(identificador);
+                    console.log(rsElement);
+                    if (rsElement.id == "correct-solution") {
+                      is_correct = true;
                     } else {
-                      spanShow.style.display = "initial";
-                      spanHide.style.display = "none";
+                      is_correct = false;
                     }
 
-                    $(`.${identificador}`).slideToggle();
-                  };
-                  spanShow.addEventListener("click", functionClickSolution);
-                  spanHide.addEventListener("click", functionClickSolution);
-                });
+                    if (is_correct == false) {
+                      //$(`div#${targetid} span:contains('Show the full solution')`).css("display", "none");
+                      newShowSpan.style.display = "none";
+                    } else {
+                      //$(`div#${targetid} span:contains('Hide the full solution')`).css("display", "none");
+                      newHideSpan.style.display = "none";
+                    }
+                    labelSolution.append(newShowSpan);
+                    labelSolution.append(newHideSpan);
 
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-              })
-              .fail(function (ex) {
-                console.log(ex);
-              });
+                    const spanShow = labelSolution.querySelector(`#show${key}`);
+                    const spanHide = labelSolution.querySelector(`#hide${key}`);
+                    const functionClickSolution = (btnE) => {
+                      const currentSpan = btnE.target;
+                      if (currentSpan.value.includes("Show")) {
+                        spanShow.style.display = "none";
+                        spanHide.style.display = "initial";
+                      } else {
+                        spanShow.style.display = "initial";
+                        spanHide.style.display = "none";
+                      }
+
+                      $(`.${identificador}`).slideToggle();
+                    };
+                    spanShow.addEventListener("click", functionClickSolution);
+                    spanHide.addEventListener("click", functionClickSolution);
+                  });
+
+                  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+                })
+                .fail(function (ex) {
+                  console.log(ex);
+                });
+            }, 1000);
           }
         });
 
