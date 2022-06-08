9fe19661f9e8016f0ec459d4fca2860440961200
Lehan <haaslewer2@gmail.com>
2022-06-08 11:19:36 +0200

    Use filter renderers, tidy up

--- view.php
+++ view.php
@@ -24,30 +24,32 @@
 
 require(__DIR__.'/../../config.php');
 require_once(__DIR__.'/lib.php');
-// Course module id.
-$id = optional_param('id', 0, PARAM_INT);
 
+use filter_siyavula\renderables\practice_activity_renderable;
+use filter_siyavula\renderables\standalone_activity_renderable;
+
+// Course module id.
+$courseid = optional_param('id', 0, PARAM_INT);
 // Activity instance id.
 $s = optional_param('s', 0, PARAM_INT);
-
 // Section id selected.
 $sectionid = optional_param('sid', null, PARAM_INT);
-$aid        = optional_param('aid', null, PARAM_RAW);
-$rid        = optional_param('rid', null, PARAM_RAW);
+$activityid = optional_param('aid', null, PARAM_RAW);
+$responseid = optional_param('rid', null, PARAM_RAW);
 
-if ($id) {
-    $cm = get_coursemodule_from_id('siyavula', $id, 0, false, MUST_EXIST);
-    $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $moduleinstance = $DB->get_record('siyavula', array('id' => $cm->instance), '*', MUST_EXIST);
+if ($courseid) {
+    $coursemodule = get_coursemodule_from_id('siyavula', $courseid, 0, false, MUST_EXIST);
+    $course = $DB->get_record('course', array('id' => $coursemodule->course), '*', MUST_EXIST);
+    $moduleinstance = $DB->get_record('siyavula', array('id' => $coursemodule->instance), '*', MUST_EXIST);
 } else {
     $moduleinstance = $DB->get_record('siyavula', array('id' => $s), '*', MUST_EXIST);
     $course = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
-    $cm = get_coursemodule_from_instance('siyavula', $moduleinstance->id, $course->id, false, MUST_EXIST);
+    $coursemodule = get_coursemodule_from_instance('siyavula', $moduleinstance->id, $course->id, false, MUST_EXIST);
 }
 
-require_login($course, true, $cm);
+require_login($course, true, $coursemodule);
 
-$modulecontext = context_module::instance($cm->id);
+$modulecontext = context_module::instance($coursemodule->id);
 
 $event = \mod_siyavula\event\course_module_viewed::create(array(
     'objectid' => $moduleinstance->id,
@@ -57,7 +59,7 @@ $event->add_record_snapshot('course', $course);
 $event->add_record_snapshot('siyavula', $moduleinstance);
 $event->trigger();
 
-$PAGE->set_url('/mod/siyavula/view.php', array('id' => $cm->id));
+$PAGE->set_url('/mod/siyavula/view.php', array('id' => $coursemodule->id));
 $PAGE->set_title(format_string($moduleinstance->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
@@ -66,13 +68,13 @@ $PAGE->requires->css('/mod/siyavula/styles.css');
 
 echo $OUTPUT->header();
 
-$info       = explode(':', $moduleinstance->subject_grade_selected);
-$subject    = $info[0];
-$grade      = $info[1];
-$clientip          = $_SERVER['REMOTE_ADDR'];
-$siyavulaconfig    = get_config('filter_siyavula');
-$baseurl            = $siyavulaconfig->url_base;
-$token              = siyavula_get_user_token($siyavulaconfig, $clientip);
+$info = explode(':', $moduleinstance->subject_grade_selected);
+$subject = $info[0];
+$grade = $info[1];
+$clientip = $_SERVER['REMOTE_ADDR'];
+$siyavulaconfig = get_config('filter_siyavula');
+$baseurl = $siyavulaconfig->url_base;
+$token = siyavula_get_user_token($siyavulaconfig, $clientip);
 $subjectgradetoc  = get_subject_grade_toc($subject, $grade, $token);
 
 // If selected one grade.
@@ -93,7 +95,7 @@ if ($moduleinstance->subject_grade_selected && $sectionid == null) {
         echo    html_writer::start_tag('div', ['class' => 'tab-content-toc']);
         foreach ($chapter->sections as $section) {
             echo html_writer::start_tag('div', ['class' => 'item-toc', 'data-sid' => $section->id]);
-            $url = new moodle_url('/mod/siyavula/view.php', ['id' => $cm->id, 'sid' => $section->id]);
+            $url = new moodle_url('/mod/siyavula/view.php', ['id' => $coursemodule->id, 'sid' => $section->id]);
             echo  html_writer::link($url, $section->title);
             echo '<div class="sv-toc__section-mastery">
                   <progress class="progress" id="section-mastery" value="'.$section->mastery.'"
@@ -115,47 +117,23 @@ if ($moduleinstance->subject_grade_selected && $sectionid == null) {
 }
 
 // If selected one section, render it.
-if ($sectionid != null && $aid === null && $rid === null) {
+if ($sectionid != null && $activityid === null && $responseid === null) {
+    $token = siyavula_get_user_token($siyavulaconfig, $clientip);
+    $usertoken = siyavula_get_external_user_token($siyavulaconfig, $clientip, $token);
+    $activitytype = 'practice';
     siyavula_update_grades($moduleinstance, $USER->id, $subjectgradetoc);
-    $usertoken = siyavula_get_external_user_token($siyavulaconfig, $clientip, $token);
 
-    $questionapi = get_activity_practice_toc($sectionid, $token, $usertoken->token, $baseurl);
-    $activityid  = $questionapi->activity->id;
-    $responseid  = $questionapi->response->id;
-    $idqt   = $questionapi->practice->section->id;
-    $seedqt = $questionapi->response->random_seed;
+    $PAGE->requires->js_call_amd('filter_siyavula/initmathjax', 'init');
 
+    $renderer = $PAGE->get_renderer('filter_siyavula');
+    $activityrenderable = new practice_activity_renderable();
+    $activityrenderable->baseurl = $baseurl;
+    $activityrenderable->token = $token;
+    $activityrenderable->usertoken = $usertoken->token;
+    $activityrenderable->activitytype = $activitytype;
+    $activityrenderable->sectionid = $sectionid;
 
-    $htmlpractice = get_html_question_practice_toc($questionapi->response->question_html,
-        $questionapi->practice->chapter->title, $questionapi->practice->chapter->mastery,
-        $questionapi->practice->section->title, $questionapi->practice->section->mastery);
-    echo $htmlpractice;
-    $PAGE->requires->js_call_amd('mod_siyavula/externalpractice', 'init', [$baseurl, $token,
-        $usertoken->token, $activityid, $responseid, $idqt, $seedqt]);
-}
-
-if ($aid != null  && $rid != null) {
-    $idqt = '';
-    $usertoken = siyavula_get_external_user_token($siyavulaconfig, $clientip, $token);
-    // Get html retry question.
-    $retryhtml = retry_question_html($aid, $rid, $token, $usertoken->token, $baseurl);
-
-    $questionapi = get_activity_practice_toc($sectionid, $token, $usertoken->token, $baseurl);
-
-    $activityid  = $retryhtml->activity->id;
-    $responseid  = $retryhtml->response->id;
-    if (isset($retryhtml->practice->section->id)) {
-        $idqt   = $retryhtml->practice->section->id;
-    }
-
-    $seedqt = $retryhtml->response->random_seed;
-
-    $htmlpractice = get_html_question_practice_toc($retryhtml->response->question_html,
-        $questionapi->practice->chapter->title, $questionapi->practice->chapter->mastery,
-        $questionapi->practice->section->title, $questionapi->practice->section->mastery);
-    echo $htmlpractice;
-    $PAGE->requires->js_call_amd('mod_siyavula/externalpractice', 'init', [$baseurl, $token,
-        $usertoken->token, $activityid, $responseid, $idqt, $seedqt]);
+    echo $renderer->render_practice_activity($activityrenderable);
 }
 
 echo $OUTPUT->footer();
