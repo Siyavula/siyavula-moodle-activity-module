620cb74c2b292e0fc0c35afa0854c319a7f15765
Marli Coetzee <mareliefies@gmail.com>
2022-07-18 09:27:45 +0200

    Trying another colour change

--- view.php
+++ view.php
@@ -89,7 +89,7 @@ if ($moduleinstance->subject_grade_selected && $sectionid == null) {
               <div class="sv-toc__section-mastery">
                 <svg style="display:none;">
                   <defs>
-                      <symbol id="fivestars" class="toc_grey">
+                      <symbol id="fivestars" class="rating toc_gray">
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="#787878" fill-rule="evenodd"/>
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="#787878" fill-rule="evenodd" transform="translate(24)"/>
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="#787878" fill-rule="evenodd" transform="translate(48)"/>
@@ -114,7 +114,7 @@ if ($moduleinstance->subject_grade_selected && $sectionid == null) {
             echo '<div class="sv-toc__section-mastery">
                     <svg style="display:none;">
                       <defs>
-                        <symbol id="fivestars" class=toc_white>
+                        <symbol id="fivestars" class=rating toc_white>
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="white" fill-rule="evenodd"/>
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="white" fill-rule="evenodd" transform="translate(24)"/>
                           <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z M0 0 h24 v24 h-24 v-24" fill="white" fill-rule="evenodd" transform="translate(48)"/>
