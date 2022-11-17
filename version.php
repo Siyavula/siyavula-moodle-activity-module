a124de88f7a71fa97f21192aee0816d57fed59ec
Lehan <lehan@siyavula.com>
2022-11-17 09:08:33 +0200

    Try again

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_siyavula';
 $plugin->release = '0.1.0';
-$plugin->version =  2022111704;
+$plugin->version =  2022111705;
 $plugin->requires = 2019111809;
 $plugin->maturity = MATURITY_STABLE;
